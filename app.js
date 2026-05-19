8d4db2020bea7a4211795d0997866433ce5a3ff7
Jamal A. Al Badi <jamalalbadi03@gmail.com>
2026-05-19 11:28:16 +0800

    Add application entry point
    Initializes Slack Bolt app in Socket Mode, creates the data store,
    registers all Slack handlers, and starts the HTTP health server.

---
+++ app.js
@@ -0,0 +1,28 @@
+import { App } from '@slack/bolt';
+import { loadConfig, validateStartupConfig } from './src/config.js';
+import { createHttpServer } from './src/http-server.js';
+import { createStore } from './src/store/index.js';
+import { registerSlackHandlers } from './src/slack/handlers.js';
+import { logger } from './src/logger.js';
+
+const config = loadConfig();
+validateStartupConfig(config);
+
+const store = await createStore(config);
+await store.init();
+
+const app = new App({
+  token: config.slack.botToken,
+  socketMode: true,
+  appToken: config.slack.appToken,
+});
+
+registerSlackHandlers(app, { config, store, logger });
+
+const httpServer = createHttpServer({ config, store, logger });
+httpServer.listen(config.port, () => {
+  logger.info('health_server_started', { port: config.port });
+});
+
+await app.start();
+logger.info('slack_app_started', { socketMode: true });
