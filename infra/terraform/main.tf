92d40b39479c3483795097ab5f181556ca7ca92f
regulus-j <jamalalbadi03@gmail.com>
2026-07-21 04:22:28 +0000

    Keep Cloud Run Socket Mode instance running

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -181,7 +181,7 @@ resource "google_cloud_run_v2_service" "app" {
   template {
     service_account = google_service_account.app.email
     scaling {
-      min_instance_count = 0
+      min_instance_count = 1
       max_instance_count = 1
     }
     max_instance_request_concurrency = 80
