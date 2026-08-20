2e31f62916c43fe3dde8bb86b11b3737985dbd7a
Jamal A. Al Badi <jamalalbadi03@gmail.com>
2026-08-21 02:26:01 +0800

    Add production recovery workflow [skip ci]

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -325,6 +325,9 @@ resource "google_cloud_run_v2_service" "app" {
   location             = var.region
   deletion_protection  = var.environment == "production"
   invoker_iam_disabled = true
+  lifecycle {
+    ignore_changes = [template[0].containers[0].image]
+  }
   template {
     service_account = google_service_account.app.email
     scaling {
