fceceb49069575433d241969ada3faedca1d6444
Jamal A. Al Badi <jamalalbadi03@gmail.com>
2026-09-01 15:03:34 +0800

    Finish low cost runtime cleanup

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -190,7 +190,7 @@ resource "google_service_account_iam_member" "github_deploy" {
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/${var.github_repository}"
 }
 resource "google_project_iam_member" "deploy_roles" {
-  for_each = toset(["roles/artifactregistry.writer", "roles/compute.instanceAdmin.v1", "roles/compute.networkAdmin", "roles/compute.osAdminLogin", "roles/compute.securityAdmin", "roles/iap.tunnelResourceAccessor", "roles/iam.serviceAccountUser", "roles/iam.workloadIdentityPoolAdmin", "roles/cloudkms.admin", "roles/logging.viewer", "roles/resourcemanager.projectIamAdmin", "roles/secretmanager.admin", "roles/serviceusage.serviceUsageAdmin", "roles/storage.admin"])
+  for_each = toset(["roles/artifactregistry.writer", "roles/compute.instanceAdmin.v1", "roles/compute.networkAdmin", "roles/compute.osAdminLogin", "roles/compute.securityAdmin", "roles/iap.tunnelResourceAccessor", "roles/iam.serviceAccountUser", "roles/iam.workloadIdentityPoolAdmin", "roles/cloudkms.admin", "roles/logging.viewer", "roles/resourcemanager.projectIamAdmin", "roles/secretmanager.admin", "roles/serviceusage.serviceUsageAdmin", "roles/storage.admin", "roles/vpcaccess.admin"])
   project  = var.project_id
   role     = each.value
   member   = "serviceAccount:${google_service_account.deploy.email}"
